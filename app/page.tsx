8a0f374d40909265717cb1efe6676d4a75ed6ad4
Yassir Mastura Utara <ymutara@addunet.ph>
2025-03-12 17:02:37 +0800

    feat: styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,27 @@ export default function Home() {
       <main className="main">
         {/* Header */}
         <div className="header">
-          <p>Pokedex</p>
-          <p>+</p>
+          <p className="logo">Pokédex</p>
+          <div className="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
 
         {/* Body */}
         <div className="body">
-          <Image src={Pokedex} alt="pokedex" height={250} width={250} />
+          <Image 
+            src={Pokedex} 
+            alt="pokedex" 
+            height={250} 
+            width={250} 
+            className="pokedex-image" 
+          />
           <div className="bodyContent">
-            <p id="title">Catch em All</p>
-            <p id="subtitle">Explore with our Interactive Pokedex</p>
-            <button style={{marginTop: "2rem"}}>View Pokedex</button>
-
+            <p id="title">Catch 'em All</p>
+            <p id="subtitle">Explore with our Interactive Pokédex</p>
+            <button className="explore-button">View Pokédex</button>
           </div>
         </div>
       </main>
